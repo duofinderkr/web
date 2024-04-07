75ea1be301a323f0147c912b555f27358ce622b8
nohkwanok <cloq@kakao.com>
2024-04-07 19:03:46 +0900

    fix anonymous user riot account error

--- project/app/views.py
+++ project/app/views.py
@@ -104,14 +104,12 @@ def get_account_by_summoner_name(request: WSGIRequest):
 def recommend_ai(request: WSGIRequest):
     user: AppUser = request.user
 
-    if not user.riot_account:
+    if not request.user.is_anonymous:
         return render(
             request,
             "recommend/ai.html",
             {
                 "user": user,
-                "riot_account": None,
-                "riot_summoner": None,
             },
         )
 
